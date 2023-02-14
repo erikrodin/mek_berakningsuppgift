a5f6259b1b7708578f2a3a681c6eaa429ac1b0a7
erikrodin <45962044+erikrodin@users.noreply.github.com>
2023-02-14 10:38:54 +0100

    Update berakning.py

--- berakning.py
+++ berakning.py
@@ -10,18 +10,22 @@ def f(t, y, l, w, m, a):
 # %% Define time spans, initial values, and constants
 
 g = 9.82 #m/s^2
-l = 1 #[1, 10] #pendulum lenghts, m
-w =  150 #[10, 70, 150] #different_frequencies, 1/s
+l = [1, 10] #pendulum lenghts, m
+w = [10, 70, 150] #different_frequencies, 1/s
 m = 1 #mass, kg
 a = 0.1 #amplitude, meter
-tspan = np.linspace(0, 0.01, 10) #timeinterval
+tspan = np.linspace(0, 10, 100) #timeinterval
 yinit = [5, 0] #initial conditions for thetha(0) & thetha´(0)
 
-
-# %% Solve differential equation
-sol = solve_ivp(lambda t, y: f(t, y, l, w, m, a),
-[tspan[0], tspan[-1]], yinit, t_eval=tspan, rtol = 1e-5)
-# %% Plot states
-for i in range(sol.y.shape[0]):
-    plt.plot(sol.t, sol.y[i])
+for i in w:
+    for j in l:
+        # %% Solve differential equation
+        sol = solve_ivp(lambda t, y: f(t, y, j, i, m, a),
+        [tspan[0], tspan[-1]], yinit, t_eval=tspan, rtol = 1e-5)
+        # %% Plot states
+        for p in range(sol.y.shape[0]):
+            plt.plot(sol.t, sol.y[0])
+        
+    
+plt.ylim([-90,90])
 plt.show(block=True)
